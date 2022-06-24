b46f2ce83794cdd679cbe47ce9e30384ac125f0c
F2731623 Eduardo Ferreira de Assis <eduardo.ferreira@bb.com.br>
2022-06-24 20:56:14 -0300

    Rename folders, new sample FolioBestSellersRpt
    - Rename folders to standardize with Cobol (uppercase).
    - Some build scripts tweaks.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Compilar
 
     cobc -x src/main.cbl -o bin/main
+
+
+# Resources
+
+http://www.csis.ul.ie/cobol/examples/default.htm
+
+https://gnucobol.sourceforge.io/faq/index.html#using-gnucobol
+
+https://github.com/mickaelandrieu/awesome-cobo
--- SRC/menu2.cbl
+++ SRC/menu2.cbl
@@ -18,10 +18,20 @@
                10 BLANK SCREEN.
                10 LINE 01 COLUMN 01 PIC X(20) ERASE EOL
                    BACKGROUND-COLOR 2 FROM WS-TITULO.
+
+       01 MENU.
+           05 LINE 07 COLUMN 15 VALUE '1 - INCLUIR'.
+           05 LINE 08 COLUMN 15 VALUE '2 - CONSULTAR'.
+           05 LINE 09 COLUMN 15 VALUE '3 - ALTERAR'.
+           05 LINE 10 COLUMN 15 VALUE '4 - EXCLUIR'.
+           05 LINE 11 COLUMN 15 VALUE '5 - RELATORIO'.
+           05 LINE 12 COLUMN 15 VALUE 'X - SAIDA'.
+           05 LINE 13 COLUMN 15 VALUE 'OPCAO...: '.
+           05 LINE 14 COLUMN 25 PIC 999 USING WS-OPCAO.
        PROCEDURE DIVISION.
            MOVE '- MENU - ' TO WS-TITULO.
            DISPLAY TELA.
-           ACCEPT WS-OPCAO AT 1610.
+           ACCEPT MENU.
            STOP RUN.
            
        END PROGRAM MAIN.
